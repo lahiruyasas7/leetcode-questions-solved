28a124fbb6b1bb8896e41a623c9fab1465420ced
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-09-10 23:41:02 +0530

    fixed find most common char question

---
+++ src/problems/easy/findMaxChar.ts
@@ -0,0 +1,20 @@
+function findMaxChar(str) {
+    let charCount = {};
+    let max = 0;
+    let maxChar = '';
+
+    for (let char of str) {
+        charCount[char] = (charCount[char] || 0) + 1;
+    }
+
+    for (let [key, value] of Object.entries(charCount)) {
+        if ((value as number) > max) {
+            max = value as number;
+            maxChar = key;
+        }
+    }
+
+    return { maxChar, max };
+}
+
+console.log(findMaxChar('mmmamaa'));
